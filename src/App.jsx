d63241dabd02e4abd03d2958f57a3f65da05ccba
Mohammed Khalid <thekhalidsha@gmail.com>
2025-04-10 11:50:04 +0530

    `Added @fullpage/react-fullpage library and configured it in the App component`

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
+import ReactFullpage from '@fullpage/react-fullpage'
 import React from 'react'
+
+const pluginWrapper = () => {
+  require('./statics/fullpage.scrollHorizontally.min');
+};
+
 
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
+    <ReactFullpage
+      pluginWrapper={pluginWrapper}
+      navigation
+      //fullpage options
+      // licenseKey={'YOUR_KEY_HERE'}
+      scrollingSpeed={500} /* Options here */
+      scrollHorizontally={true}  /* Because we are using the extension */
+      scrollHorizontallyKey={'YOUR KEY HERE'}
+
+      render={({ state, fullpageApi }) => {
+        return (
+          <ReactFullpage.Wrapper>
+            <div className="section">
+              <p>Section 1 (welcome to fullpage.js)</p>
+              <button onClick={() => fullpageApi.moveSectionDown()}>
+                Click me to move down
+              </button>
+            </div>
+            <div className="section">
+              <p>Section 2</p>
+            </div>
+          </ReactFullpage.Wrapper>
+        );
+      }}
+    />
+  );
+
 }
 
 export default App
