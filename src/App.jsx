f71cb02184029518c429278308b69a2fe742f4d6
Mohammed Khalid <thekhalidsha@gmail.com>
2025-04-10 11:55:00 +0530

    `Removed pluginWrapper and scrollHorizontally options, updated scrollingSpeed and licenseKey options`

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import ReactFullpage from '@fullpage/react-fullpage'
 import React from 'react'
-import pluginWrapper from './statics/fullpage.scrollHorizontally.min';
 
 
 
@@ -8,31 +7,27 @@ import pluginWrapper from './statics/fullpage.scrollHorizontally.min';
 const App = () => {
   return (
     <ReactFullpage
-      pluginWrapper={pluginWrapper}
-      navigation
-      //fullpage options
-      // licenseKey={'YOUR_KEY_HERE'}
-      scrollingSpeed={500} /* Options here */
-      scrollHorizontally={true}  /* Because we are using the extension */
-      scrollHorizontallyKey={'YOUR KEY HERE'}
+    //fullpage options
+    licenseKey = {'YOUR_KEY_HERE'}
+    scrollingSpeed = {1000} /* Options here */
 
-      render={({ state, fullpageApi }) => {
-        return (
-          <ReactFullpage.Wrapper>
-            <div className="section">
-              <p>Section 1 (welcome to fullpage.js)</p>
-              <button onClick={() => fullpageApi.moveSectionDown()}>
-                Click me to move down
-              </button>
-            </div>
-            <div className="section">
-              <p>Section 2</p>
-            </div>
-          </ReactFullpage.Wrapper>
-        );
-      }}
-    />
-  );
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section">
+            <p>Section 1 (welcome to fullpage.js)</p>
+            <button onClick={() => fullpageApi.moveSectionDown()}>
+              Click me to move down
+            </button>
+          </div>
+          <div className="section">
+            <p>Section 2</p>
+          </div>
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />
+);
 
 }
 
