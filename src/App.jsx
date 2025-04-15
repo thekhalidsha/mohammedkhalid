e21bdae8aaf339c24c804ccbbd2da326dead7a8d
Mohammed Khalid <thekhalidsha@gmail.com>
2025-04-15 10:55:24 +0530

    `Removed unused components and layouts from the project`

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import PublicLayout from "./layouts/PublicLayout";
-import TestPages from "./pages/TestPages";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<PublicLayout />}>
-            <Route index element={<HomePage />} />
-          </Route>
-          <Route path="/test" element={<TestPages/>}/>
+          <Route path="/" element={<>Hello World</>}/>
         </Routes>
       </BrowserRouter>
     </>
