155f8214dd3d240f255d6ecc180718a01ca12e30
Mohammed Khalid <thekhalidsha@gmail.com>
2025-04-21 10:38:34 +0530

    `Update NotFound.jsx: change "page" to "site" in 404 error message`

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -8,7 +8,7 @@ const NotFound = () => {
             <Navbar />
             <header className="max-w-screen-xl px-4 py-8 mx-auto lg:py-16 w-full border-zinc-200 border rounded-2xl">
                 <h1 className="font-heading font-semibold text-center font-incognito tracking-tight sm:text-6xl text-3xl lg:leading-[3.7rem] leading-tight mt-6 mb-3">Error 404!</h1>
-                <p className="text-center text-base dark:text-zinc-400 text-zinc-600 leading-relaxed">Oopsies! This page does not exist on this page.</p>
+                <p className="text-center text-base dark:text-zinc-400 text-zinc-600 leading-relaxed">Oopsies! This page does not exist on this site.</p>
             </header>
             <Footer className={`md:text-center`}/>
         </section>
